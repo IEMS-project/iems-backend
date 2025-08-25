2f3e14c4a3f9787a199c6832a5305b6b5789c3ec
ThinhVo0 <xuanthinhhh605@gmail.com>
2025-08-25 15:20:06 +0700

    feat: update module department

---
+++ department-service/src/main/java/com/iems/departmentservice/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.iems.departmentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+}
--- department-service/src/main/java/com/iems/departmentservice/repository/DepartmentRepository.java
+++ department-service/src/main/java/com/iems/departmentservice/repository/DepartmentRepository.java
@@ -2,6 +2,12 @@ package com.iems.departmentservice.repository;
 
 import com.iems.departmentservice.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
+import java.util.UUID;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+    boolean existsByDepartmentName(String departmentName);
+    boolean existsByDepartmentNameAndIdNot(String departmentName, UUID id);
 }
